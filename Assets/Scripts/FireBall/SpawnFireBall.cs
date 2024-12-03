4ae4b7b4edccb85803c1eaa8c47f46d64ebd2a00
Joaco <lopezjoaquin488@gmail.com>
2024-12-02 21:04:12 -0300

    scripts

---
+++ Assets/Scripts/FireBall/SpawnFireBall.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFireBall : MonoBehaviour
+{
+    public GameObject vfx;
+    public Transform startPoint;
+    public Transform endPoint;
+    void Start()
+    {
+        var starPos = startPoint.position;
+        GameObject objVFX = Instantiate(vfx, starPos, Quaternion.identity) as GameObject;
+
+        var endPos = endPoint.position;
+
+        RotateTo(objVFX, endPos);
+    }
+
+    void RotateTo (GameObject obj, Vector3 destination)
+    {
+        var direction = destination - obj.transform.position;
+        var rotation = Quaternion.LookRotation(direction);
+        obj.transform.localRotation = Quaternion.Lerp(obj.transform.rotation, rotation, 1);
+    }
+
+}
